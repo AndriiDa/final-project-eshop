386d37dfae14dc0c8154c9334f65fbae18df2974
Nazar Denysiak <nazden009@gmail.com>
2019-10-08 23:38:10 +0300

    add bootswatch, bootstrap deps

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,15 @@ import './App.scss';
 const App = () => {
   console.log('hi');
 
-  return <div className="test">Something is working!</div>;
+  return <div>
+    <button type="button" className="btn btn-primary">Primary</button>
+    <button type="button" className="btn btn-secondary">Secondary</button>
+    <button type="button" className="btn btn-success">Success</button>
+    <button type="button" className="btn btn-info">Info</button>
+    <button type="button" className="btn btn-warning">Warning</button>
+    <button type="button" className="btn btn-danger">Danger</button>
+    <button type="button" className="btn btn-link">Link</button>
+  </div>;
 };
 
 export default App;
