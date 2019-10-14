2c7a728fb7d95b4f34c74598f90dc2c00a213101
Vova Shkil <vshkil@gmail.com>
2019-10-14 10:09:46 -0400

    tests to GenderTest added

--- src/test/java/com/fs7/finalproject/eshop/model/GenderTest.java
+++ src/test/java/com/fs7/finalproject/eshop/model/GenderTest.java
@@ -1,16 +1,20 @@
 package com.fs7.finalproject.eshop.model;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class GenderTest {
 
     @Test
-    public void getShortName() {
+    public void getShortNameEqualsMWhenGenderIsMale() {
+        Gender gender = Gender.MALE;
+        Assert.assertEquals("M", gender.getShortName());
     }
 
     @Test
-    public void fromShortName() {
+    public void getShortNameEqualsFWhenGenderIsFemale() {
+        Gender gender = Gender.FEMALE;
+        Assert.assertEquals("F", gender.getShortName());
     }
+
 }
