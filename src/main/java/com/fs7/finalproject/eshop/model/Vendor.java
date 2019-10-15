fd2d74b6fea93fb1453792c1abe1a660cd1b80f8
Andrii Danilishin <a.i.danilishin@gmail.com>
2019-10-16 00:15:02 +0300

    add vendors fields

--- src/main/java/com/fs7/finalproject/eshop/model/Vendor.java
+++ src/main/java/com/fs7/finalproject/eshop/model/Vendor.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "VENDORS")
@@ -17,4 +18,29 @@ public class Vendor {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(updatable = false)
   private Long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "notes")
+  private String notes;
+
+  @Column(name = "is_active")
+  private Boolean is_active;
+
+  @Column(name = "cr_time")
+  private Date cr_time;
+
+  @Column(name = "cr_user_id")
+  private Long cr_user_id;
+
+  @Column(name = "lm_time")
+  private Date lm_time;
+
+  @Column(name = "lm_user_id")
+  private Long lm_user_id;
+
+
+
+
 }
