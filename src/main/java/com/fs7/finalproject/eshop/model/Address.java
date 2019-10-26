2eaec63724cd1f388a380660bb40d39dc53d0af7
Vova Shkil <vshkil@gmail.com>
2019-10-26 08:55:40 -0400

    Address Entity added to models

--- src/main/java/com/fs7/finalproject/eshop/model/Address.java
+++ src/main/java/com/fs7/finalproject/eshop/model/Address.java
@@ -1,6 +1,5 @@
 package com.fs7.finalproject.eshop.model;
 
-import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.Column;
@@ -13,13 +12,27 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "ADDRESSES")
 @Data
-@Builder
 public class Address {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(updatable = false)
+  @Column(name = "ID", updatable = false)
   private Long id;
 
-  @Column(nullable = false)
+  @Column(name = "COUNTRY", nullable = true, length = 100)
+  private String country;
+
+  @Column(name = "STATE", nullable = true, length = 100)
+  private String state;
+
+  @Column(name = "CITY", nullable = true, length = 100)
+  private String city;
+
+  @Column(name = "STREET", nullable = true, length = 100)
+  private String street;
+
+  @Column(name = "ZIP_CODE", nullable = true, length = 8)
+  private String zipCode;
+
+  @Column(name = "ADDRESS_LINE", nullable = true, length = 250)
   private String addressLine;
 }
