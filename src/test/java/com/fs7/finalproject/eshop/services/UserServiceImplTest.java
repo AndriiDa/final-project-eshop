c28dac67da01086b835860c76eae470d7cec5d74
vovashkil <34689562+vovashkil@users.noreply.github.com>
2019-10-26 08:16:02 -0400

    Add property value entities (#37)
    * add all Entities, full describe User entity
    
    * change User entity and User tests
    
    * change Property, PropertyValue entity and tests
    
    * added empty tests to PropertyTest to pass coveralls check
    
    * added test templates for services
    
    * tests for services added

--- src/test/java/com/fs7/finalproject/eshop/services/UserServiceImplTest.java
+++ src/test/java/com/fs7/finalproject/eshop/services/UserServiceImplTest.java
@@ -1,5 +1,23 @@
+package com.fs7.finalproject.eshop.services;
+
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UserServiceImplTest {
 
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
 }
