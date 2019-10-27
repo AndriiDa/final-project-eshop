4fb2e90a796c2a5acf18c342e9ebd7b6c6db21ce
Vova Shkil <vshkil@gmail.com>
2019-10-27 05:49:15 -0400

    fetchCategoryStart -> fetchCategoryStarted fix3

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchCategoryStarted } from './redux/category/category.actions';
+import {connect} from 'react-redux';
+import {fetchCategoryStarted} from './redux/category/category.actions';
 import './App.scss';
 
-const App = ({ fetchCategoryStarted }) => {
-  useEffect(() => {
-    fetchCategoryStarted();
-  }, []);
+const App = ({fetchCategoryStarted}) => {
+    useEffect(() => {
+        fetchCategoryStarted();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  return <p>Working!</p>;
+    return <p>Working!</p>;
 };
 
 App.propTypes = {
-  fetchCategoryStarted: PropTypes.func.isRequired
+    fetchCategoryStarted: PropTypes.func.isRequired
 };
 
 const mapDispatchToProps = dispatch => ({
-  fetchCategoryStarted: () => dispatch(fetchCategoryStarted())
+    fetchCategoryStarted: () => dispatch(fetchCategoryStarted())
 });
 
 export default connect(
-  null,
-  mapDispatchToProps
+    null,
+    mapDispatchToProps
 )(App);
