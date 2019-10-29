a2f69e638bf016d6ed266b49339f3202f262c8bc
o.demyanenko <olgadem@hotmail.com>
2019-10-29 19:44:30 +0200

    added Cart Entity

--- src/main/java/com/fs7/finalproject/eshop/model/Cart.java
+++ src/main/java/com/fs7/finalproject/eshop/model/Cart.java
@@ -1,19 +1,38 @@
 package com.fs7.finalproject.eshop.model;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "CARTS")
 @Data
-@NoArgsConstructor
-public class Cart extends BaseEntity{
+public class Cart {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(updatable = false)
+  private Long id;
 
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "USER_ID", referencedColumnName = "ID", nullable = false,
+          foreignKey = @ForeignKey(name = "FK_CARTS_USER_ID"))
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "PRODUCT_ID", referencedColumnName = "ID", nullable = false,
+          foreignKey = @ForeignKey(name = "FK_CARTS_PRODUCT_ID"))
+  private Product product;
+
+  @Column(name = "QUANTITY", nullable = false)
+  private int quantity;
 }
