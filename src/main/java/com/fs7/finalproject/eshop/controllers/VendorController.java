5774658a14aa4e78fe61809d9b83ee83dfc61355
vovashkil <34689562+vovashkil@users.noreply.github.com>
2019-10-31 06:57:22 -0400

    Update VendorController.java
    added import for Put-/Post-/DeleteMapping

--- src/main/java/com/fs7/finalproject/eshop/controllers/VendorController.java
+++ src/main/java/com/fs7/finalproject/eshop/controllers/VendorController.java
@@ -5,6 +5,9 @@ import com.fs7.finalproject.eshop.services.VendorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
