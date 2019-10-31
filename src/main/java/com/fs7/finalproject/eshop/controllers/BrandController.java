7f57528e8ec11e46ad43b8c67d450a5be211929f
vovashkil <34689562+vovashkil@users.noreply.github.com>
2019-10-31 06:00:42 -0400

    Update BrandController.java
    add import entities to avoid use *

--- src/main/java/com/fs7/finalproject/eshop/controllers/BrandController.java
+++ src/main/java/com/fs7/finalproject/eshop/controllers/BrandController.java
@@ -4,7 +4,9 @@ import com.fs7.finalproject.eshop.model.Vendor;
 import com.fs7.finalproject.eshop.services.VendorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
