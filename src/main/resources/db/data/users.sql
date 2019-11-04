43602c1dac79bd15c78706fde609bc271d2e3167
Vova Shkil <vshkil@gmail.com>
2019-11-04 10:52:53 -0500

    folder for *.sql schema and data sql files is created and configured

---
+++ src/main/resources/db/data/users.sql
@@ -0,0 +1,29 @@
+INSERT INTO USERS (ID, ADDRESS_ID, BIRTH_DATE, EMAIL, EMAIL_VERIFIED, FIRST_NAME
+           ,GENDER, IS_ACTIVE, LAST_NAME, LOGIN_NAME, LOGIN_PASSWORD, MIDDLE_NAME, PHONE_NUMBER
+           ,ROLE, VERIFICATION_CODE)
+VALUES (1, 1, '1987-10-23', 'ivanov@gmail.com', 1, 'Іван', 'M', 1, 'Іванов', 'ivanov', 'ivanov123',
+        'Семенович', '+380502346578', 'A', '2345');
+
+INSERT INTO USERS (ID, ADDRESS_ID, BIRTH_DATE, EMAIL, EMAIL_VERIFIED, FIRST_NAME
+                  ,GENDER, IS_ACTIVE, LAST_NAME, LOGIN_NAME, LOGIN_PASSWORD, MIDDLE_NAME, PHONE_NUMBER
+                  ,ROLE, VERIFICATION_CODE)
+VALUES (2, 2, '1998-01-22', 'petrov@gmail.com', 1, 'Петро', 'M', 1, 'Петров', 'petrov', 'petrov123', 'Іванович',
+        '+380508765434', 'A', '0987');
+
+INSERT INTO USERS (ID, ADDRESS_ID, BIRTH_DATE, EMAIL, EMAIL_VERIFIED, FIRST_NAME
+                  ,GENDER, IS_ACTIVE, LAST_NAME, LOGIN_NAME, LOGIN_PASSWORD, MIDDLE_NAME, PHONE_NUMBER
+                  ,ROLE, VERIFICATION_CODE)
+VALUES (3, 3, '1977-09-03', 'semenov@gmail.com', 1, 'Віктор', 'M', 1, 'Семенов', 'semenov', 'semenov123',
+        'Олексійович', '+380672346578', 'A', '0023');
+
+INSERT INTO USERS (ID, ADDRESS_ID, BIRTH_DATE, EMAIL, EMAIL_VERIFIED, FIRST_NAME
+                  ,GENDER, IS_ACTIVE, LAST_NAME, LOGIN_NAME, LOGIN_PASSWORD, MIDDLE_NAME, PHONE_NUMBER
+                  ,ROLE, VERIFICATION_CODE)
+VALUES (4, 4, '2000-01-12', 'solomon@gmail.com', 1, 'Жанна', 'F', 1, 'Соломонівна', 'solomonivna', 'zhanna123',
+        'Василівна', '+380507776543', 'A', '6654');
+
+INSERT INTO USERS (ID, ADDRESS_ID, BIRTH_DATE, EMAIL, EMAIL_VERIFIED, FIRST_NAME
+                  ,GENDER, IS_ACTIVE, LAST_NAME, LOGIN_NAME, LOGIN_PASSWORD, MIDDLE_NAME, PHONE_NUMBER
+                  ,ROLE, VERIFICATION_CODE)
+VALUES (5, 5, '1998-12-01', 'kolyada@gmail.com', 1, 'Степан', 'M', 1, 'Коляда', 'kolyada', 'kolyada123',
+        'Афанасович', '+380509987654', 'C', '1123');
