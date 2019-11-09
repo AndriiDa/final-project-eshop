935588168664ba686eae02e3b73a46e422b0373e
Tim <luckystudydanit@gmail.com>
2019-11-09 14:35:06 +0200

    feature/PETPJFS7-240 -- Has been added router -- Has been added flex-container -- Has been added universal structure for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchCategoryStart } from './redux/category/category.actions';
+// React components
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeComponent from "./pages/Home/home.component";
+// Redux
+import { Provider } from 'react-redux';
+import { store } from "./redux/store";
+// styles
 import './App.scss';
 
-const App = ({ fetchCategoryStarted }) => {
-  useEffect(() => {
-    fetchCategoryStarted();
-  }, []);
-
-  return <p>Working!</p>;
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={HomeComponent}/>
+                    <Route render={() => <div></div>}/>
+                </Switch>
+            </Router>
+        </Provider>
+    );
 };
 
-App.propTypes = {
-  fetchCategoryStarted: PropTypes.func.isRequired
-};
+export default App;
 
-const mapDispatchToProps = dispatch => ({
-  fetchCategoryStarted: () => dispatch(fetchCategoryStart())
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(App);
+// import { connect } from 'react-redux';
+// import { fetchCategoryStart } from './redux/category/category.actions';
+// const App = ({ fetchCategoryStarted }) => {
+//   useEffect(() => {
+//     fetchCategoryStarted();
+//   }, []);
+//
+//   return <p>Working!</p>;
+// };
+//
+// App.propTypes = {
+//   fetchCategoryStarted: PropTypes.func.isRequired
+// };
+//
+// const mapDispatchToProps = dispatch => ({
+//   fetchCategoryStarted: () => dispatch(fetchCategoryStart())
+// });
+//
+// export default connect(
+//   null,
+//   mapDispatchToProps
+// )(App);
