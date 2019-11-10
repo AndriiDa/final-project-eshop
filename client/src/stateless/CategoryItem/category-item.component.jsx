bc7dc0f0aff6bb1eed0515d0c292c81d44e364d8
Tim <luckystudydanit@gmail.com>
2019-11-10 14:32:46 +0200

    feature/PETPJFS7-242 -- Resolve conflicts

--- client/src/stateless/CategoryItem/category-item.component.jsx
+++ client/src/stateless/CategoryItem/category-item.component.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as ArrowIcon } from "../../assets/images/icons/arrow.svg
 const CategoryItemComponent = ({ categoryName: { name, imgUrl } }) => {
   return (
     <li className="d-flex justify-content-between well">
-      <img src={imgUrl} />
+      <img src={imgUrl}  alt={name} />
       <span className="list-item-align">{ name }</span>
       <ArrowIcon/>
     </li>
