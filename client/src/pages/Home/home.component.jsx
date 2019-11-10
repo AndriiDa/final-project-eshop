a49cd8029ad01992c9104e9d1e89b5b4f6dc7a0a
DennyTim <44003977+DennyTim@users.noreply.github.com>
2019-11-10 17:09:17 +0200

    PERPJFS7 - 242 (#55)
    * feature/Front1-3
    
    feature/[Front#1] - Has been created react projec
    feature/[Front#2] - Has been configurate eslint
    feature/[Front#3] - Has been added node-sass
    -- Has been added some changes to .gitignore and readme
    
    * PETPJFS7-240 --Has been added store and service for fetching
    
    * feature/PETPJFS7-240
    -- Has been added router
    -- Has been added flex-container
    -- Has been added universal structure for components
    
    * feature/PETPJFS7-240
    -- Hotfix add useEffect
    
    * feature/PETPJFS7-242
    -- Hotfix, add .gitkeep
    
    * feature/PETPJFS7-242
    -- Added sidebar
    
    * feature/PETPJFS7-242
    -- Resolve conflicts

--- client/src/pages/Home/home.component.jsx
+++ client/src/pages/Home/home.component.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { FlexContainer } from "../../stateless/FlexContainer/flex-container.styles";
+
 import { connect } from "react-redux";
 import { fetchCategoryStart } from "../../redux/category/category.actions";
+import SidebarComponent from "./components/Sidebar/sidebar.component";
 
-const HomeComponent = ({fetchCategoryStarted}) => {
+const HomeComponent = ({ categoryItems, fetchCategoryStarted }) => {
     useEffect(() => {
         fetchCategoryStarted();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
-        <FlexContainer>Home</FlexContainer>
+        <Fragment>
+            <SidebarComponent elements={ categoryItems.length && categoryItems }/>
+        </Fragment>
     );
 };
 
@@ -19,8 +22,15 @@ HomeComponent.propTypes = {
     fetchCategoryStarted: PropTypes.func.isRequired
 };
 
+const mapStateToProps = (state) => ({
+    categoryItems: state.category.categoryItems
+});
+
 const mapDispatchToProps = dispatch => ({
     fetchCategoryStarted: () => dispatch(fetchCategoryStart())
 });
 
-export default connect(null, mapDispatchToProps)(HomeComponent);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(HomeComponent);
