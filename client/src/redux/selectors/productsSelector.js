ddc9c0dc93784b8f66d73b8e64c92a7beffbd3ed
Vova Shkil <vshkil@gmail.com>
2020-01-22 04:22:09 -0500

    reselect in ProductsSelector used

---
+++ client/src/redux/selectors/productsSelector.js
@@ -0,0 +1,34 @@
+import {createSelector} from "reselect";
+
+const getProductsSelector = (state) => {
+    return state.productsPage.products;
+};
+
+export const getProducts = createSelector (getProductsSelector,
+    (products) => {
+    return products.filter(item => true);
+});
+
+export const getPageSize = (state) => {
+    return state.productsPage.pageSize;
+};
+
+export const getTotalItemsCount = (state) => {
+    return state.productsPage.totalItemsCount;
+};
+
+export const getCurrentPageNumber = (state) => {
+    return state.productsPage.currentPageNumber;
+};
+
+export const getIsLoadingInProgress = (state) => {
+    return state.productsPage.isLoadingInProgress;
+};
+
+export const getTogglingAddRemoveCartButtonInProgress = (state) => {
+    return state.productsPage.togglingAddRemoveCartButtonInProgress;
+};
+
+export const getCart = (state) => {
+    return state.productsPage.cart;
+};
